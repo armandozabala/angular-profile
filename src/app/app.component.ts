b7b8382edfe9de87bd2fef6407a05db0df7325e9
armandozabala <armandozabala@gmail.com>
2019-07-26 10:50:41 -0500

    4 files modified 🤪

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+ name = 'Alejandra Giraldo';
+ email='email@gmail.com';
+ profesion='Ingeniero';
+
+ img = 'https://pbs.twimg.com/profile_images/1057449615841214466/KXT83JRQ_400x400.jpg';
+
+  following = 5001;
+  followers = 10002;
+
+  strengths = [{ text: 'English', level: 'Basic' }, { text: 'HTML', level: 'High' }, { text: 'Testing', level: 'Low' }];
+
+  people = [
+    {
+      "name": "Vanessa M.",
+      "age": 16,
+      "color": 'red'
+    },
+    {
+      "name": "Carlos Angulo",
+      "age": 25,
+      "color": 'green'
+    },
+    {
+      "name": "Maleja",
+      "age": 21,
+      "color": 'pink'
+    }
+  ];
+
+
 }
